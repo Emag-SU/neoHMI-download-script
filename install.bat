539d9840b6f6d51d416b04fb88770b4f8179b939
Alberto Macchi <amacchi@emag.com>
2025-07-17 12:21:16 +0200

    ADD: variable for not deleting file

--- install.bat
+++ install.bat
@@ -17,7 +17,26 @@ call setup.bat 1
 
 echo "neoHMI installed!"
 pause
+
+
+:check_exit
+set _args=0
+for %%x in (%*) do set /a _args+=1
+
+if %_args% equ 0 goto delete
+    
+if %1%==1 goto exit
+goto delete
+
+
+:delete
 (goto) 2>nul & del "%~f0"
+
+
+:exit
+exit /b
+
+
 :error_folder_already_exists
 echo "Error! neoHMI is already installed in this system! If you're trying to update run the integrated update.bat"
 goto check_exit
