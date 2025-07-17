cc5c0e597b154f095d181683a847bda4ed358320
Alberto Macchi <amacchi@emag.com>
2025-07-17 12:20:54 +0200

    ADD: check if already installed

--- install.bat
+++ install.bat
@@ -3,6 +3,9 @@
 echo "Installing neoHMI..."
 
 
+set install_folder="C:\neoHMI"
+if exist "%install_folder%\" goto error_folder_already_exists
+
 git clone https://github.com/Emag-SU/neoHMI.git
 
 mkdir C:\neoHMI
@@ -15,3 +18,6 @@ call setup.bat 1
 echo "neoHMI installed!"
 pause
 (goto) 2>nul & del "%~f0"
+:error_folder_already_exists
+echo "Error! neoHMI is already installed in this system! If you're trying to update run the integrated update.bat"
+goto check_exit
