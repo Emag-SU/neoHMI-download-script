206fdfb8328b726f35578d143b18e6d6d83605b1
Alberto Macchi <amacchi@emag.com>
2025-07-17 11:00:06 +0200

    ADD: echo + call invece che start

--- install.bat
+++ install.bat
@@ -1,8 +1,17 @@
 @echo off
+
+echo "Installing neoHMI..."
+
+
 git clone https://github.com/Emag-SU/neoHMI.git
+
 mkdir C:\neoHMI
 move neoHMI C:\neoHMI
 cd C:\neoHMI\neoHMI
-start C:\neoHMI\neoHMI\setup.bat
-echo "neoHMI installata"
+
+call setup.bat
+
+
+echo "neoHMI installed!"
+pause
 exit
